332b8b1c32e71c918aa757421829537096833124
Mikaely19 <125111409306@ulife.com.br>
2025-05-05 23:43:39 -0300

    Update Exercicio10.java

--- Exercicio10.java
+++ Exercicio10.java
@@ -3,26 +3,19 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int[] vetor = new int[10];
 
-        // Preencher o vetor com números fornecidos pelo usuário
-        System.out.println("Digite 10 números inteiros:");
-        for (int i = 0; i < vetor.length; i++) {
+        System.out.println("Digite 10 números:");
+        for (int i = 0; i < vetor; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             vetor[i] = scanner.nextInt();
         }
 
-        // Ordenar o vetor usando Bubble Sort
-        for (int i = 0; i < vetor.length - 1; i++) {
-            for (int j = 0; j < vetor.length - 1 - i; j++) {
+        for (int i = 0; i < vetor - 1; i++) {
                 if (vetor[j] > vetor[j + 1]) {
-                    // Trocar os elementos de posição
+                
                     int temp = vetor[j];
-                    vetor[j] = vetor[j + 1];
-                    vetor[j + 1] = temp;
                 }
-            }
         }
 
-        // Exibir o vetor ordenado
         System.out.println("Vetor ordenado em ordem crescente:");
         for (int num : vetor) {
             System.out.print(num + " ");
